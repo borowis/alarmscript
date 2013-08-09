686994f488c815f387cf558ea542dbfb93affa07
Borys Zibrov <zebrahere@mail.ru>
2013-08-09 23:42:11 +0300

    Update play.sh

--- play.sh
+++ play.sh
@@ -1,1 +1,1 @@
-/usr/bin/X11/xterm -display :0 -bg black -fg white -e '/usr/bin/mplayer -softvol -volume 100 -shuffle -loop 0 -playlist "/home/borowis/Documents/alarm/alarm0.1/AlarmSongs.m3u"'
+/usr/bin/X11/xterm -display :0 -bg black -fg white -e '/usr/bin/mplayer -softvol -volume 100 -shuffle -loop 0 -playlist "./AlarmSongs.m3u"'
